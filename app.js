4d1c830ead4ae4efb0d37f2eab1d643dbf59123a
Juan Manuel Gonzalez <juanmagonzalez577@gmail.com>
2022-07-23 02:33:41 -0300

    quinto commit segunda entrega

--- app.js
+++ app.js
@@ -1,53 +1,8 @@
-//Arreglo de productos 
-/* const arr1 = [
-    {nombre: "paleta", precio: 15000, descripcion: "wilson"},
-    {nombre: "paleta", precio: 30000, descripcion: "bullpadel"},
-    {nombre: "paleta", precio: 45000, descripcion: "adiddas"},
-    {nombre: "zapatillas", precio: 10000, descripcion: "fila"},
-    {nombre: "zapatillas", precio: 9000, descripcion: "topper"},
-    {nombre: "zapatillas", precio: 12000, descripcion: "adiddas"},
-]; */
-//Mostrar los productos de la lista completa
-/*     for (const producto of arr1){
-    console.log(producto.nombre);
-    }
-
-
-
- */
-//Consultar disponibilidad del producto
-//const prodDisponible = prompt("Ingrese producto a verificar disponibilidad").toLowerCase();
-//const result1 = arr1.some(elemento => elemento.nombre = prodDisponible);
- //       if(result1){
- //           alert("Producto disponible");
- //          //Filtrar para conocer el stock de ese producto
- //           const productoFiltrado = arr1.filter((elemento) => elemento.nombre == prodDisponible);
- //           alert(`El stock del prodcuto ${prodDisponible} es: ${productoFiltrado.length}`); /*error! si coloco zapatillas me sale que hay 4 en stock*/ //y si pongo .length-1 en paleta me sale 2
- //           console.log(productoFiltrado.length);
- //       }else{
- //           alert("Prodcuto no disponible");
- //       }
-
-//Agregar productos 
-/* const productosAgregados = parseInt(prompt("En caso de ser adm ingrese la cantidad de productos a agregar al listado"));
-const mensaje2 = alert(`La cantidad de productos que vas a ingresar es de ${productosAgregados}`)
-for(let i = 0; i < productosAgregados; i++){
-    let nombre = prompt("Ingrese el nombre");
-    let precio = prompt("Ingrese el precio");
-    let descripcion = prompt("Ingrese la descripción");
-    
-    arr1.push({nombre: nombre, precio: precio, descripcion: descripcion});
-    
-}
-
- */
-
 
 /*INTERECACCIÓN CON HTML****************************************************************************/
-//seccion admin
+
 
 let container = document.getElementById("divFormAdmin");
-/* let prodcutoAgregar = prompt("Ingrese el nombre del producto para agregarlo"); */
 let listaProductos = [
     {id: 1, nombre: "paleta", precio: 15000, descripcion: "wilson", imagen: "./img/wilson.png"},
     {id: 2, nombre: "paleta", precio: 30000, descripcion: "bullpadel", imagen: "./img/bullpadel.png"},
@@ -57,13 +12,11 @@ let listaProductos = [
     {id: 6, nombre: "zapatillas", precio: 12000, descripcion: "adiddas", imagen: "./img/z-adidas.png"},
 ];
 
-
-/* listaProductos.push(prodcutoAgregar); */
 for(const producto of listaProductos){
     let item = document.createElement("div");
     item.innerHTML = `<h2>${producto.nombre}</h2>
                       <h4>${producto.descripcion}</h4>  
-                    <b>Precio: ${producto.precio}</b>
+                    <b>Precio: $ ${producto.precio}</b>
                     <h6>ID: ${producto.id}</h6>
                     <img src="${producto.imagen}">
                     `;
@@ -72,25 +25,71 @@ for(const producto of listaProductos){
     
 }
 
+//seccion admin
+
+//sessionStorage
+let user;
+const array = [];
+let userStorage = sessionStorage.getItem("usuario");
+let paisUserStorage = sessionStorage.getItem("pais");
+
+if(userStorage){
+    user = userStorage;
+    pais = paisUserStorage;
+    let response = `Bienvenido ${user}`;
+
+    alert(response);
+}else {
+    user = prompt("Ingrese su nombre").toLowerCase();
+    pais = prompt("Ingrese su país")
+   
+   
+} 
+let nombreCompletoUser = sessionStorage.setItem("usuario", user);
+let paisCompletoUser = sessionStorage.setItem("pais", pais);
+/* console.log(nombreCompletoUser);
+console.log(typeof(nombreCompletoUser)); */
+//localStorage - guardar productos 
+localStorage.setItem("listaProductos", JSON.stringify(listaProductos));
+
+const h2divUsuario = document.getElementById("h2divUsuario");
+const h4divUsuario = document.getElementById("h4divUsuario");
+h2divUsuario.innerHTML += user;
+h4divUsuario.innerHTML += pais;
+
+
+
+
+
+//funcion para actualizar precio - admin
+function actPrecio (id, precio){
+    let productoStorage = JSON.parse(localStorage.getItem("listaProductos"));
+    let productoEncontrado = productoStorage.find(elemento => elemento.id === id);
+    productoEncontrado.precio = precio;
+
+    localStorage.setItem("listaProductos", JSON.stringify(productoStorage));
+}
+
+actPrecio(2, 3000);
+
+//primer modelo para consultar productos a consultar disponibilidad
 const botonClickAca = document.getElementById("btnProducto1");
-/* function respuestaClick(){
-    console.log("respuesta click");
-} */
-botonClickAca.addEventListener("click", () => console.log("Hiciste click en ver ofertas"));
 
-/* let nombre = document.getElementById("nombre"); */
-/* nombre.addEventListener("keyup", (e) => {
-    if(e.key != "Enter"){
-        
-    }else{
-        alert("presiono enter")
-        
-    }
-}); */
+botonClickAca.addEventListener("click", (e) => {
+   console.log("Hiciste click en ver disponibilidad del producto");
+   let mensaje1 = parseInt(prompt("¿Que producto deseas filtrar? 1-Paletas 2-Zapatillas"));
+   console.log(mensaje1);
+   if (mensaje1 === 1){
+    alert("Hiciste click en Paletas");
+   }else if (mensaje1 === 2){
+    alert("Hiciste click en Zapatillas");
+   }else {
+    alert("No seleccionaste correctarmente, apretar F5");
+   }
+});
 
 
-//evento change
-//nombre.addEventListener("change", () => console.log("Te saliste del campo"));
+
 
 //evento input
 let maxDigitos = document.getElementById("maxDigitos");
@@ -114,7 +113,7 @@ form.addEventListener("submit", (e) => {
     if (info.children[3].value.includes("@") && info.children[3].value.includes(".")){
         alert("formulario enviado");
     }else if(!info.children[3].value.includes("@")){
-        alert("El campo campo Email no contiene @");
+        alert("El campo Email no contiene @");
         info.children[3].value = "";
         console.log("Correo electrónico incorrecto");
     }
