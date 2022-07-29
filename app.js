eeeffac875c95bb39c4410dd4e04ba20808ad768
Juan Manuel Gonzalez <juanmagonzalez577@gmail.com>
2022-07-29 02:20:21 -0300

    Se incorpora agregar al carrito y modal

--- app.js
+++ app.js
@@ -3,48 +3,90 @@
 
 //lista de prodcutos 
 
-let container = document.getElementById("divFormAdmin");
-let listaProductos = [
+//let container = document.getElementById("divFormAdmin");
+/*  listaProductos = [
     {id: 1, nombre: "paleta", precio: 15000, descripcion: "wilson", imagen: "./img/wilson.png"},
     {id: 2, nombre: "paleta", precio: 30000, descripcion: "bullpadel", imagen: "./img/bullpadel.png"},
     {id: 3, nombre: "paleta", precio: 45000, descripcion: "adiddas", imagen: "./img/adiddas.png"},
     {id: 4, nombre: "zapatillas", precio: 10000, descripcion: "fila", imagen: "./img/fila.png"},
     {id: 5, nombre: "zapatillas", precio: 9000, descripcion: "topper", imagen: "./img/zapatillas toper.png"},
     {id: 6, nombre: "zapatillas", precio: 12000, descripcion: "adiddas", imagen: "./img/z-adidas.png"},
-];
-//iterador para crear producto
+]; */
+/* 
 for(const producto of listaProductos){
     let item = document.createElement("div");
-    item.innerHTML = `<h2>${producto.nombre}</h2>
-                      <h4>${producto.descripcion}</h4>  
-                    <b>Precio: $ ${producto.precio}</b>
-                    <h6>ID DE PRODUCTO: ${producto.id}</h6>
-                    <img src="${producto.imagen}">
+    item.innerHTML = `<h2 class="titleProduct">${producto.nombre}</h2>
+                      <h4 class="DescripcionProduct">${producto.descripcion}</h4>  
+                    <b class="precioProduct">$ ${producto.precio}</b>
+                    <h6 class="idProduct">ID DE PRODUCTO: ${producto.id}</h6>
+                    <img  class="imgProduct" src="${producto.imagen}">
                     <button class="btnAgregarCarrito" id= ${producto.id}>Agregar al carrito</button>
                     `;
     item.className += "productosCarrito";
-
+   
     container.append(item);
-    
+} */
+
+
+
+
+//PRIMER PASO, INYECTAR HTML
+
+const contenedorProductos = document.getElementById('contenedor-productos');
+const contenedorCarrito = document.getElementById("carrito-contenedor");
+let carrito = [];
+listaProductos.forEach((producto) =>{
+  const div = document.createElement('div');
+  div.classList.add('producto');
+  div.innerHTML = `
+                <img src=${producto.img} class="imgProducto">
+                <h3>${producto.nombre}</h3>
+                <p>Descripcion: ${producto.descripcion}</p>
+                <p class="precioProducto">Precio:$${producto.precio}</p>
+                <button id="agregar${producto.id}" class="boton-agregar">Agregar</button>
+  `
+  contenedorProductos.appendChild(div);
+  const botonAgregar = document.getElementById(`agregar${producto.id}`);
+  botonAgregar.addEventListener('click', (e) =>{
+    agregarCarrito(producto.id);
+  })
+  
+})
+
+const agregarCarrito = (prodId) => {
+  const item = listaProductos.find((prod) => prod.id === prodId);
+  carrito.push(item);
+  console.log(carrito);
+  actCarrito();
 }
 
+//eliminar del carrito
 
+const eliminarDelCarrito = (prodId) =>{
+  const item2 = carrito.find((prod) => prod.id === prodId);
+  const indice = carrito.indexOf(item2);
+  carrito.splice(indice, 1);
+  actCarrito();
+}
 
-//boton accion agregar al carrito
+//agregar al carrito
 
-const array = [];
+const actCarrito = () => {
+  contenedorCarrito.innerHTML="";
+  carrito.forEach((prod) => {
+    const div2 = document.createElement("div");
+    div2.className =  ("productoEnCarrito");
+    div2.innerHTML = `
+                    <p>${prod.nombre}</p>
+                    <p>${prod.descripcion}</p>
+                    <p>Precio:$ ${prod.precio}</p>
+                    <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+                    <button onclick ="eliminarDelCarrito(${prod.id})" class="boton-eliminar">X</button>
+                    `
+                    contenedorCarrito.appendChild(div2);
+  })
 
- let btnAgregar = document.querySelectorAll(".btnAgregarCarrito");
-btnAgregar.forEach(add)
-
-
-
-
-
-
-
-
-
+}
 
 
 //seccion admin
