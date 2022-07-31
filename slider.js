0ce4ef667a328fe174de4bcd46469a0840de8e39
Juan Manuel Gonzalez <juanmagonzalez577@gmail.com>
2022-07-31 13:37:29 -0300

    se termina banner

--- slider.js
+++ slider.js
@@ -8,10 +8,36 @@ slider.insertAdjacentElement('afterbegin', sliderSectionLast) //para traer el ul
 
 //funciones
 
-function trasladarR(){
+function traslateR(){
     let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition ="none";
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    }, 500);
 }
 
 function traslateL(){
-
+    
+    let sliderSection = document.querySelectorAll(".slider__section");
+    let sliderSectionLast = sliderSection[sliderSection.length-1];
+    slider.style.marginLeft = "0%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition ="none";
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+        slider.style.marginLeft = "-100%";
+    }, 500);
 }
+btnSliderR.addEventListener("click", function(){
+    traslateR();
+})
+btnSliderL.addEventListener("click", function(){
+    traslateL();
+})
+//para que avance automaticamente sin click
+setInterval(function(){
+    traslateR();
+}, 5000);
