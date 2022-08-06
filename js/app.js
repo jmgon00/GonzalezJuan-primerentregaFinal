9c9101a5abbc6dfe0b7412a2f9200083cb3b936c
Juan Manuel Gonzalez <juanmagonzalez577@gmail.com>
2022-08-06 20:34:22 -0300

    se agrega fetch

--- js/app.js
+++ js/app.js
@@ -35,20 +35,55 @@ for(const producto of listaProductos){
 const contenedorProductos = document.getElementById('contenedor-productos');
 const contenedorCarrito = document.getElementById("carrito-contenedor");
 const botonVaciar = document.getElementById("vaciar-carrito");
+const botonFinalizar = document.getElementById("finalizar-compra");
 const contadorCarrito = document.getElementById("contadorCarrito");
 const precioTotal = document.getElementById("precioTotal");
+/* const usuarioLogin = document.getElementById("form-usuario-acceso"); */
 
 let carrito = [];
 
-//localStorage
-/* document.addEventListener('DOMContentLoaded', () => {
-  if(localStorage.getItem('carrito')){
-    carrito = JSON.parse(localStorage.getItem('carrito'))
-    actCarrito();
-  }
-
+/* listaProductos.forEach((producto) =>{
+  const div = document.createElement('div');
+  div.classList.add('producto');
+  div.innerHTML = `
+                <img src=${producto.img} class="imgProducto">
+                <h3>${producto.nombre}</h3>
+                <p>${producto.descripcion}</p>
+                <p class="precioProducto">Precio:$${producto.precio}</p>
+                <button id="agregar${producto.id}" class="boton-agregar">Agregar</button>
+  `
+  contenedorProductos.appendChild(div);
+  const botonAgregar = document.getElementById(`agregar${producto.id}`);
+  botonAgregar.addEventListener('click', (e) =>{
+    agregarCarrito(producto.id);
+  })
   
 }) */
+
+
+//JSON
+
+fetch("/js/stock.json")
+.then(response => response.json())
+.then(productos =>{
+  productos.forEach((producto) =>{
+    const div = document.createElement('div');
+    div.classList.add('producto');
+    div.innerHTML = `
+                  <img src=${producto.img} class="imgProducto">
+                  <h3>${producto.nombre}</h3>
+                  <p>${producto.descripcion}</p>
+                  <p class="precioProducto">Precio:$${producto.precio}</p>
+                  <button id="agregar${producto.id}" class="boton-agregar">Agregar</button>
+                  
+                  `
+    contenedorProductos.appendChild(div);
+    const botonAgregar = document.getElementById(`agregar${producto.id}`);
+    botonAgregar.addEventListener('click', (e) =>{
+      agregarCarrito(producto.id);
+    })
+    
+  })
 
 //vaciar carrito 
 botonVaciar.addEventListener("click", () =>{
@@ -71,32 +106,12 @@ botonVaciar.addEventListener("click", () =>{
   })
   carrito.length = 0;
   actCarrito();
-})
-
-
-
-
-
-listaProductos.forEach((producto) =>{
-  const div = document.createElement('div');
-  div.classList.add('producto');
-  div.innerHTML = `
-                <img src=${producto.img} class="imgProducto">
-                <h3>${producto.nombre}</h3>
-                <p>Descripcion: ${producto.descripcion}</p>
-                <p class="precioProducto">Precio:$${producto.precio}</p>
-                <button id="agregar${producto.id}" class="boton-agregar">Agregar</button>
-  `
-  contenedorProductos.appendChild(div);
-  const botonAgregar = document.getElementById(`agregar${producto.id}`);
-  botonAgregar.addEventListener('click', (e) =>{
-    agregarCarrito(producto.id);
-  })
   
 })
 
+
 const agregarCarrito = (prodId) => {
-  const item = listaProductos.find((prod) => prod.id === prodId);
+  const item = productos.find((prod) => prod.id === prodId);
   carrito.push(item);
   actCarrito();
   const Toast = Swal.mixin({
@@ -138,103 +153,48 @@ const actCarrito = () => {
     div2.innerHTML = `
                     <p>${prod.nombre}</p>
                     <p>${prod.descripcion}</p>
-                    <p>Precio:$ ${prod.precio}</p>
+                    <p>${prod.precio}</p>
                     <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
-                    <button onclick ="eliminarDelCarrito(${prod.id})" class="boton-eliminar">X</button>
+                    <button onclick ="eliminarDelCarrito(${prod.id})" class="boton-eliminar">X</button> 
                     `
                     contenedorCarrito.appendChild(div2);
                     
   })
   contadorCarrito.innerText = carrito.length;
   precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0);
+ 
   
 }
 
 
+//*ERROR LINEA 155*//
+//ERROR LINEA 161//
 
+//finalizar compra
 
-
-//seccion admin
-//sessionStorage
-/* let user;
-let pais;
-let userStorage = sessionStorage.getItem("usuario");
-let paisUserStorage = sessionStorage.getItem("pais");
-
-if(userStorage){
-    user = userStorage;
-    pais = paisUserStorage;
-    let response = `Bienvenido de nuevo ${user}`;
+botonFinalizar.addEventListener("click", () =>{
+  Swal.fire({
+    title: 'Estas muy cerca de finalizar tu compra',
+    
+    icon: 'success',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Finalizar compra'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Tus productos ya están en camino',
+        'Te estará llegando un correo electrónico con toda la información',
+        'Realizado con éxito'
+      )
+    }
+  })
+  carrito.length = 0;
+  actCarrito();
  
-    Swal.fire({
-        title: response,
-        showClass: {
-          popup: 'animate__animated animate__fadeInDown'
-        },
-        hideClass: {
-          popup: 'animate__animated animate__fadeOutUp'
-        }
-      })
-}else {
-    user = prompt("Ingrese su nombre");
-    pais = prompt("Ingrese su país")
-   
-} 
- */
-/* 
-let nombreCompletoUser = sessionStorage.setItem("usuario", user);
-let paisCompletoUser = sessionStorage.setItem("pais", pais); */
-/* console.log(nombreCompletoUser);
-console.log(typeof(nombreCompletoUser)); */
 
-//localStorage - guardar productos 
-//localStorage.setItem("listaProductos", JSON.stringify(listaProductos));
-
-
-//imprimir los datos que se ingresan por prompt en el html user
-/* const h2divUsuario = document.getElementById("h2divUsuario");
-const h4divUsuario = document.getElementById("h4divUsuario");
-const btnRestablecerDatosUser = document.getElementById("btnRestablecerDatosUser");
-h2divUsuario.innerHTML += user;
-h4divUsuario.innerHTML += pais;
- */
-
-
-//boton accion  restablecer datos usuario en sesion
-/* btnRestablecerDatosUser.addEventListener("click", (e) => {
-    h2divUsuario.innerText = "";
-    h4divUsuario.innerText = "";
-    sessionStorage.clear();
-    Swal.fire({
-        title: 'Estas seguro/a?',
-        
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          Swal.fire(
-            'Eliminado!',
-            'Exliminado con exito, presiona F5',
-            'success'
-          )
-        }
-      })
-
-});
- */
-//boton accion ingreso como admin
-//funcion para actualizar precio - admin
-/* function actPrecio (id, precio){
-  let productoStorage = JSON.parse(localStorage.getItem("listaProductos"));
-  let productoEncontrado = productoStorage.find(elemento => elemento.id === id);
-  productoEncontrado.precio = precio;
-
-  localStorage.setItem("listaProductos", JSON.stringify(productoStorage));
-}; */
-
+})
 
 
 //PRIMER MODELO para consultar productos a consultar disponibilidad
@@ -246,17 +206,36 @@ botonClickAca.addEventListener("click", (e) => {
    
   
    let mensaje1 = parseInt(prompt("¿Que producto deseas filtrar? 1-Paletas 2-Zapatillas"));
- /* 
+  
    if (mensaje1 === 1){
     alert("Hiciste click en Paletas");
    }else if (mensaje1 === 2){
     alert("Hiciste click en Zapatillas");
    }else {
     alert("No seleccionaste correctarmente, apretar F5");
-   } */
-  mensaje1 = mensaje1 === 1 ? alert("Hiciste click en Paletas") : alert("Hiciste click en Zapatillas");
+   }
+  //mensaje1 = mensaje1 === 1 ? alert("Hiciste click en Paletas") : alert("Hiciste click en Zapatillas");
+//});
+
+
+
+
+//funciones asincronas y localStorage
+
+/* function cargarLocalStorage(){
+  return new Promise ((resolve, reject)=>{
+    console.log("Cargando productos..");
+    setTimeout(()=>{
+      resolve(productos);
+      localStorage.getItem("productos");
+    }, 1500)
+  })
+}
+
+cargarLocalStorage().then(datos=>{
+  console.log(datos);
 });
+ */
 
-
-
+})})
 
